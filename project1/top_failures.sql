fd6465923fa26386d5445857a092b4a18c790bd3
ozlop <oswaldo.t.lopez@gmail.com>
2018-11-27 15:20:25 -0800

    Add top failures query to Logs Analysis project

---
+++ project1/top_failures.sql
@@ -0,0 +1,10 @@
+SELECT day
+    , count(errors) / (count(*) * 1.0) AS failure
+FROM (
+    SELECT date(time) AS day
+        , CASE WHEN status = '404 NOT FOUND' THEN 1 END errors
+    FROM log
+) log
+GROUP BY day
+HAVING count(errors) / (count(*) * 1.0) > .01
+ORDER BY day;
