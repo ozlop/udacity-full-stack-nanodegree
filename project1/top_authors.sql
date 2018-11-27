c40de91f6961d3c124a5bdcd169bdf6ee667a94c
ozlop <oswaldo.t.lopez@gmail.com>
2018-11-27 15:17:59 -0800

    Add top authors query to Logs Analysis project

---
+++ project1/top_authors.sql
@@ -0,0 +1,17 @@
+    WITH visited
+    AS (
+        SELECT replace(path, '/article/', '') AS path
+            , count(path) AS total
+        FROM log
+        WHERE path LIKE '/article/%'
+            AND status = '200 OK'
+        GROUP BY path
+        )
+    SELECT authors.name, sum(visited.total) AS TOTAL
+    FROM visited
+    JOIN articles
+        ON visited.path = articles.slug
+    JOIN authors
+        ON articles.author = authors.id
+    GROUP BY authors.name
+    ORDER BY total DESC;
