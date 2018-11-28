e8ccd74d678b9d7a68bb3fc56fdb860c2dec0d32
ozlop <oswaldo.t.lopez@gmail.com>
2018-11-28 15:34:25 -0800

    Correct indentation in top authors SQL script

--- project1/top_authors.sql
+++ project1/top_authors.sql
@@ -1,17 +1,17 @@
-    WITH visited
-    AS (
-        SELECT path
-            , count(path) AS total
-        FROM log
-        WHERE path LIKE '/article/%'
-            AND status = '200 OK'
-        GROUP BY path
-        )
-    SELECT authors.name, sum(visited.total) AS TOTAL
-    FROM visited
-    JOIN articles
-        ON visited.path = CONCAT('/article/', articles.slug)
-    JOIN authors
-        ON articles.author = authors.id
-    GROUP BY authors.name
-    ORDER BY total DESC;
+WITH visited
+AS (
+    SELECT path
+        , count(path) AS total
+    FROM log
+    WHERE path LIKE '/article/%'
+        AND status = '200 OK'
+    GROUP BY path
+    )
+SELECT authors.name, sum(visited.total) AS TOTAL
+FROM visited
+JOIN articles
+    ON visited.path = CONCAT('/article/', articles.slug)
+JOIN authors
+    ON articles.author = authors.id
+GROUP BY authors.name
+ORDER BY total DESC;
